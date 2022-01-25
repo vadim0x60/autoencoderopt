81a5707015540a103901330ab7f85ebae3292fd2
Vadim Liventsev <hello@vadim.me>
2022-01-25 03:09:09 +0300

    Weights and Biases

--- opt.py
+++ opt.py
@@ -2,12 +2,10 @@ import os
 import requests
 from pathlib import Path
 import json as j
-import pickle
 import itertools
+import wandb
 import nevergrad as ng
 from random import sample
-from heapq import heappush, heappushpop
-from uuid import uuid4
 
 COMPILE_SERVER_API = os.environ.get('COMPILE_SERVER_API') or 'https://tree2tree.app/api'
 LATENT_DIM = int(os.environ.get('LATENT_DIM') or '150')
@@ -22,25 +20,6 @@ RANGE = int(os.environ.get('RANGE') or '6')
 datasets_path = Path(__file__).parent / 'datasets'
 solutions_path = Path(__file__).parent / 'solutions'
 os.makedirs(solutions_path, exist_ok=True)
-
-class SourceFile():
-    def __init__(self, code):
-        self.filename = f'{uuid4()}.cpp'
-        self.persist = False
-        with open(solutions_path / self.filename, 'w') as f:
-            f.write(code)
-
-    def __lt__(self, other):
-        return self.filename < other.filename
-
-    def __del__(self):
-        if not self.persist:
-            os.remove(solutions_path / self.filename)
-
-    def read(self):
-        with open(solutions_path / self.filename, 'r') as f:
-            return f.read()
-
 class BackendError(Exception):
     pass
 
@@ -53,11 +32,11 @@ def backend(method, path, json=None):
         raise BackendError from e
 
 class Executable():
-    def __init__(self, source_file):
-        self.source_file = source_file
+    def __init__(self, source):
+        self.source = source
         
     def __enter__(self):
-        self.binary_name = backend('POST', '/programs', json={'program': self.source_file.read()})['binary']
+        self.binary_name = backend('POST', '/programs', json={'program': self.source})['binary']
         return self
 
     def __exit__(self, exc_type, exc_value, trace):
@@ -126,37 +105,59 @@ def decode_vector(vector):
         'top_p': 0
     }
 
-    return SourceFile(backend('POST', '/decode', json=decoding_request)['program'])
+    return backend('POST', '/decode', json=decoding_request)['program']
 
-best_programs = []
+def evaluate_vector(vector):
+    try:
+        program = decode_vector(vector)
+        fitness = test_program(program, test_cases)
+    except BackendError:
+        fitness = MIN_FITNESS
+    return program, fitness
 
-for i in range(TOP_K + 1):
-    heappush(best_programs, (MIN_FITNESS, SourceFile(str(i))))
+best_fitness = MIN_FITNESS
 
-def unfitness_function(task):
-    def test_vector(vector):
-        global best_fitness
-        test_cases = sample_tests(task)
-        try:
-            program = decode_vector(vector)
-            fitness = test_program(program, test_cases)
-            heappushpop(best_programs, (fitness, program))
-        except BackendError:
-            fitness = MIN_FITNESS
-        return - fitness
+def make_report(optimizer, candidate, program, fitness):
+    global best_fitness
+    best_fitness = max(fitness, best_fitness)
 
-    return test_vector
+    report = {
+        "#num-ask": optimizer.num_ask,
+        "#num-tell": optimizer.num_tell,
+        "#num-tell-not-asked": optimizer.num_tell_not_asked,
+        "#lineage": candidate.heritage["lineage"],
+        "#generation": candidate.generation,
+        "#parents_uids": [],
+        "#program": program,
+        "#fitness": fitness,
+        "#best_fitness": best_fitness
+    }  
+
+    if isinstance(candidate._meta.get("sigma"), float):
+        report["#meta-sigma"] = candidate._meta["sigma"] 
+    if candidate.generation > 1:
+        report["#parents_uids"] = candidate.parents_uids
+    return report
+
+wandb.init(project='autoencoderopt')
+wandb.config = {name: globals()[name] for name in ['LATENT_DIM', 'MAX_TESTS', 'TOP_K', 'TASK', 'OPTIMIZER', 'BUDGET', 'RANGE']}
 
 experiment = f'{TASK}-{OPTIMIZER.__name__}'
-
 optimizer = OPTIMIZER(parametrization=ng.p.Array(shape=(LATENT_DIM,), lower=-RANGE, upper=RANGE), budget=BUDGET)
 
 try:
-    recommendation = optimizer.minimize(unfitness_function(TASK), verbosity=2)
-finally:
-    with open(solutions_path / f'{experiment}.json', 'w') as f:
-        j.dump({program.filename: fitness for fitness, program in best_programs}, f)
+    for _ in range(optimizer.budget):
+        candidate = optimizer.ask()
+        test_cases = sample_tests(TASK)
+        program, fitness = evaluate_vector(candidate.value)
 
-    for fitness, program in best_programs:
-        if fitness > MIN_FITNESS:
-            program.persist = True
+        wandb.log(make_report(optimizer, candidate, program, fitness))
+
+        optimizer.tell(candidate, - fitness)
+finally:
+    recommendation = optimizer.provide_recommendation()
+    program, fitness = evaluate_vector(recommendation.value)
+    wandb.log(make_report(optimizer, recommendation, program, fitness))
+    program = '\n'.join([backend('GET', '/imports'), program])
+    with open(solutions_path / f'{experiment}-{fitness}.json') as f:
+        j.dump(program, f)
