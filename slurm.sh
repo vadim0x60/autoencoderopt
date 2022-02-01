da7c3fd4cade77772fe31114e047c670332b253c
Vadim Liventsev <hello@vadim.me>
2022-02-01 04:22:47 +0300

    Increased SLURM time limit

--- slurm.sh
+++ slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=autoencoderopt    # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --time=48:00:00               # Time limit hrs:min:sec
+#SBATCH --time=96:00:00               # Time limit hrs:min:sec
 #SBATCH --partition=mcs.default.q
 
 export TASK=$(ls datasets | sed -n $SLURM_ARRAY_TASK_ID'p')
